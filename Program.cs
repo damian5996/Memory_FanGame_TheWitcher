f3c2cb8f4530a15604791d45e21cb5ac774811c3
damian5996 <damian5996@wp.pl>
2016-12-11 23:55:11 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace WindowsFormsApplication1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void playSound()
+        {
+            SoundPlayer sound = new SoundPlayer(@"Lullaby.wav");
+            sound.PlayLooping();
+        }
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            playSound();
+            Application.Run(new Start());
+        }
+    }
+}
